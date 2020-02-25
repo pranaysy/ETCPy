24b88c1e10a86ffb09c81447419313ea5da6f02d
Pranay S. Yadav <pranay6481@gmail.com>
2020-02-25 19:18:12 +0530

    Formatted all code using Black

--- ETC/ETC.py
+++ ETC/ETC.py
@@ -11,25 +11,27 @@ from .utils import warmup, check_equality
 
 warmup()
 
+
 def ETC(x):
-            
+
     etc = 0
-    
+
     while not check_equality(x):
-        
+
         x = one_step(x)
         etc += 1
-    
-    print(f'>> Effort To Compress (ETC) = {etc}')
+
+    print(f">> Effort To Compress (ETC) = {etc}")
     return etc
 
+
 def nETC(x, etc=None):
-    
+
     if etc:
-        etc *= 1/len(x)
-        print(f'>> Normalized Effort To Compress (nETC) = {etc}')
+        etc *= 1 / len(x)
+        print(f">> Normalized Effort To Compress (nETC) = {etc}")
         return etc
     else:
-        etc = ETC(x)/len(x)
-        print(f'>> Normalized Effort To Compress (nETC) = {etc}')
+        etc = ETC(x) / len(x)
+        print(f">> Normalized Effort To Compress (nETC) = {etc}")
         return etc
--- ETC/NSRPS.py
+++ ETC/NSRPS.py
@@ -10,48 +10,55 @@ import numpy as np
 from numba import njit
 from .utils import check_equality, check_sanity
 
+
 def _get_all_pairs(x):
-    
+
     y = np.vstack((x[:-1], x[1:]))
     M = np.int((y.max() + 1))
-    
-    uniqs, invind, counts = np.unique(M*y[0] + y[1], return_counts=True, return_inverse=True)
-       
-    matr = np.zeros(M*M)
+
+    uniqs, invind, counts = np.unique(
+        M * y[0] + y[1], return_counts=True, return_inverse=True
+    )
+
+    matr = np.zeros(M * M)
     matr[uniqs] = counts
-    uniqc = matr.reshape(M,M)
+    uniqc = matr.reshape(M, M)
 
     return uniqs, invind, uniqc
 
+
 @njit
 def _get_freq_pair_idx(x):
-    
+
     uniqs, invind, uniqc = x
     np.fill_diagonal(uniqc, 0)
-    
+
     return np.where(invind == np.where(uniqs == uniqc.argmax())[0])[0]
+
 
 @njit
 def _substitute(x, idx):
-    
+
     y = x.copy()
     y[idx] = y.max() + 1
     filtidx = np.full(y.shape, True)
-    filtidx[idx+1] = False
-    
+    filtidx[idx + 1] = False
+
     return y[filtidx]
 
+
 def one_step(x):
-    
+
     return _substitute(x, _get_freq_pair_idx((_get_all_pairs(x))))
 
+
 def NSRPS(x):
-    
+
     if not check_sanity(x):
         return False
-    
+
     if check_equality(x):
-        print('> Constant symbolic sequence, nothing to substitute ...')
+        print("> Constant symbolic sequence, nothing to substitute ...")
         return None
     else:
         return one_step(x)
