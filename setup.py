be44b7f82ea9adcf0352bab8d3d72406f6b4e8e4
Pranay S. Yadav <mail@pranaysy.com>
2021-03-10 15:05:39 +0530

    Switch parallel compute to joblib

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     ),
     include_dirs=[numpy.get_include()],
     name="ETCPy",
-    version="1.3.4",
+    version="1.3.5",
     author_email="mail@pranaysy.com",
     description="Compute the Effort-To-Compress (ETC) of a symbolic sequence",
     packages=find_packages(),
