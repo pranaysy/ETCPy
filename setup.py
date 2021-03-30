04053e4b1a035f7dc2064b0449be39a30909c592
Pranay S. Yadav <mail@pranaysy.com>
2021-03-30 13:17:50 +0530

    Updated setup script with metadata and dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,15 @@
 
 @author: Pranay S. Yadav
 """
+DESCRIPTION = "Compute the Effort-To-Compress (ETC) of symbolic sequences"
+DISTNAME = 'ETCPy'
+PLATFORMS = ['posix']
+MAINTAINER = "Pranay S. Yadav"
+MAINTAINER_EMAIL = "mail@pranaysy.com"
+URL = 'https://github.com/pranaysy/ETCPy/'
+LICENSE = "Apache License, Version 2.0"
+DOWNLOAD_URL = 'https://github.com/pranaysy/ETCPy/'
+VERSION = '1.3.5'
 
 from setuptools import setup, find_packages
 from Cython.Build import cythonize
@@ -22,10 +31,26 @@ setup(
         compiler_directives={"language_level": "3"},
     ),
     include_dirs=[numpy.get_include()],
-    name="ETCPy",
-    version="1.3.5",
-    author_email="mail@pranaysy.com",
-    description="Compute the Effort-To-Compress (ETC) of a symbolic sequence",
+    name=DISTNAME,
+    version=VERSION,
+    author=MAINTAINER,
+    author_email=MAINTAINER_EMAIL,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    platforms=PLATFORMS,
+    url=URL,
+    download_url=DOWNLOAD_URL,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
     packages=find_packages(),
-    license="Apache License, Version 2.0",
+    license=LICENSE,
+    install_requires = ["numpy", "cython"],
+    classifiers=[
+              'Intended Audience :: Science/Research',
+              'Programming Language :: Python :: 3',
+              'License :: OSI Approved :: Apache Software License',
+              'Topic :: Scientific/Engineering :: Mathematics',
+              'Operating System :: POSIX',
+              'Operating System :: Unix',
+              'Operating System :: MacOS'],
 )
